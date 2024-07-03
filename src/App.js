86639e2bc58153e4c577a5aaa1d8bda8979d3ca8
karlyitianli <karl.yitian.li@gmail.com>
2024-07-03 14:19:38 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+import './fonts.css';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
+import { MuiTypography } from './components/MuiTypography';
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <MuiTypography />
+        {/* <FormControl>
+            <InputLabel htmlFor="my-input">Email address</InputLabel>
+            <Input id="my-input" aria-describedby="my-helper-text" />
+          </FormControl>
+          <Box sx={{ width: '100%' }}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+              <Tabs>
+                <Tab label="Typography"/>
+                <Tab label="Item Two"/>
+                <Tab label="Item Three"/>
+              </Tabs>
+            </Box>
+          </Box>
+          <Button variant="text" style={{ textTransform: 'none' }}>text</Button>
+          <Button variant="contained" style={{ textTransform: 'none' }}>Contained</Button>
+          <Button variant="outlined" style={{ textTransform: 'none' }}>Outlined</Button>
+          <TextField id="standard-basic" label="Standard" variant="standard" /> */}
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Typography from '@mui/material/Typography';
+
+export const MuiTypography = () => {
+    return (
+        <div>
+            <Typography variant="h1">
+                H1 - 96px
+            </Typography>
+            <Typography variant="h2">
+                H2 - 60px
+            </Typography>
+            <Typography variant="h3">
+                H3 - 48px
+            </Typography>
+            <Typography variant="h4">
+                H4 - 34px
+            </Typography>
+            <Typography variant="h5">
+                H5 - 24px
+            </Typography>
+            <Typography variant="h6">
+                H6 - 20px
+            </Typography>
+            <Typography variant="subtitle1">
+                Subtitle 1 - 16px
+            </Typography>
+            <Typography variant="subtitle2">
+                Subtitle 2 - 14px
+            </Typography>
+            <Typography variant="body1">
+                Body 1 - 16px
+            </Typography>
+            <Typography variant="body2">
+                Body 2 - 14px
+            </Typography>
+            <Typography variant="button">
+                Button - 14px
+            </Typography>
+            <Typography variant="caption">
+                Caption - 12px
+            </Typography>
+            <Typography variant="overline">
+                Overline - 12px
+            </Typography>
+        </div>
+
+    )
+}
